2c1973c134bd42bb362134243bf5c7704162d8ed
yujin mac <kimyu28jin@gmail.com>
2021-02-20 14:02:21 +0900

    HomeButton 클래스 생성

--- build/main.js
+++ build/main.js
@@ -2,12 +2,30 @@
 var headerBtns = document.querySelector('.header_list');
 var darkSection = document.querySelector('.dark');
 var modalSection = document.querySelector('.modal');
-var openModal = function (e) {
-    var target = e.target;
-    if (target.tagName === 'BUTTON') {
-        darkSection === null || darkSection === void 0 ? void 0 : darkSection.classList.remove('hide');
-        modalSection === null || modalSection === void 0 ? void 0 : modalSection.classList.remove('hide');
+// 각각 버튼에게 부여해보자.
+var HomeButton = /** @class */ (function () {
+    function HomeButton() {
+        this.open = function (e) {
+            var target = e.target;
+            if (target.tagName === 'BUTTON') {
+                darkSection === null || darkSection === void 0 ? void 0 : darkSection.classList.remove('hide');
+                modalSection === null || modalSection === void 0 ? void 0 : modalSection.classList.remove('hide');
+            }
+        };
+        this.close = function (e) {
+            var target = e.target;
+            if (target.className === 'close' || target.tagName === 'SVG') {
+                darkSection === null || darkSection === void 0 ? void 0 : darkSection.classList.add('hide');
+                modalSection === null || modalSection === void 0 ? void 0 : modalSection.classList.add('hide');
+            }
+        };
     }
+    return HomeButton;
+}());
+// 모달에서 add함수를 만들면 되는건가?
+// 추가되는 리스트(content)는 객체로 생성해야함
+var openModal = function () {
+    //변경중
 };
 headerBtns === null || headerBtns === void 0 ? void 0 : headerBtns.addEventListener("click", openModal);
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,30 @@ const headerBtns = document.querySelector<HTMLElement>('.header_list');
 const darkSection = document.querySelector<HTMLElement>('.dark');
 const modalSection = document.querySelector<HTMLElement>('.modal');
 
-const openModal = (e:Event) => {
+// 각각 버튼에게 부여해보자.
+
+class HomeButton {
+    open = (e:Event) => {
     const target: HTMLElement = <HTMLElement>e.target;
    if(target.tagName === 'BUTTON') {
     darkSection?.classList.remove('hide');
     modalSection?.classList.remove('hide');
-   }
+      }
+    }
+    close = (e:Event) => {
+    const target: HTMLElement = <HTMLElement>e.target;
+   if(target.className === 'close' || target.tagName === 'SVG') {
+    darkSection?.classList.add('hide');
+    modalSection?.classList.add('hide');
+      }
+    }
+}
+// 모달에서 add함수를 만들면 되는건가?
+// 추가되는 리스트(content)는 객체로 생성해야함
+
+
+const openModal = () => {
+    //변경중
 }
 
 headerBtns?.addEventListener("click", openModal);
