dd3e28be5dc31d6a1e06df381e9a50e592d52d4b
yujin mac <kimyu28jin@gmail.com>
2021-03-03 14:53:25 +0900

    HomeButton 클래스 수정, 생성
    - Content, Label 타입 추가
    - getLabel 함수 추가
    - HomeButton 클래스 이용하여 객체 생성

--- build/main.js
+++ build/main.js
@@ -2,30 +2,54 @@
 var headerBtns = document.querySelector('.header_list');
 var darkSection = document.querySelector('.dark');
 var modalSection = document.querySelector('.modal');
-// 각각 버튼에게 부여해보자.
 var HomeButton = /** @class */ (function () {
-    function HomeButton() {
-        this.open = function (e) {
-            var target = e.target;
-            if (target.tagName === 'BUTTON') {
-                darkSection === null || darkSection === void 0 ? void 0 : darkSection.classList.remove('hide');
-                modalSection === null || modalSection === void 0 ? void 0 : modalSection.classList.remove('hide');
-            }
-        };
-        this.close = function (e) {
-            var target = e.target;
-            if (target.className === 'close' || target.tagName === 'SVG') {
-                darkSection === null || darkSection === void 0 ? void 0 : darkSection.classList.add('hide');
-                modalSection === null || modalSection === void 0 ? void 0 : modalSection.classList.add('hide');
-            }
-        };
+    function HomeButton(attr) {
+        this.attr = attr;
     }
+    HomeButton.prototype.open = function (e) {
+        var target = e.target;
+        if (target.tagName === 'BUTTON') {
+            darkSection === null || darkSection === void 0 ? void 0 : darkSection.classList.remove('hide');
+            modalSection === null || modalSection === void 0 ? void 0 : modalSection.classList.remove('hide');
+        }
+    };
+    HomeButton.prototype.close = function (e) {
+        var target = e.target;
+        if (target.className === 'close' || target.tagName === 'SVG') {
+            darkSection === null || darkSection === void 0 ? void 0 : darkSection.classList.add('hide');
+            modalSection === null || modalSection === void 0 ? void 0 : modalSection.classList.add('hide');
+        }
+    };
+    HomeButton.prototype.getLabel = function () {
+        var thisAttr = this.attr;
+        switch (thisAttr) {
+            case 'image':
+            case 'video':
+                return {
+                    title: '제목',
+                    link: '링크'
+                };
+            case 'note':
+            case 'task':
+                return {
+                    title: '제목',
+                    memo: '내용'
+                };
+        }
+    };
     return HomeButton;
 }());
 // 모달에서 add함수를 만들면 되는건가?
 // 추가되는 리스트(content)는 객체로 생성해야함
+var imageBtn = new HomeButton('image');
+var videoBtn = new HomeButton('video');
+var noteBtn = new HomeButton('note');
+var taskBtn = new HomeButton('task');
 var openModal = function () {
-    //변경중
+    // switch(a) {
+    //   case 'video':
+    //     alert ('fsdf');
+    // }
 };
 headerBtns === null || headerBtns === void 0 ? void 0 : headerBtns.addEventListener("click", openModal);
 //# sourceMappingURL=main.js.map
