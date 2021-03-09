3e99783c766046f278072774a5332356ba37a793
yujin mac <kimyu28jin@gmail.com>
2021-03-09 16:34:26 +0900

    page,image component 수정

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,20 +1,14 @@
+import { BaseComponent } from './../component.js';
 
-export class PageComponent {
-    private element: HTMLUListElement;
+export class PageComponent extends BaseComponent<HTMLUListElement> {
     
     // PageComponent는 constructor에 ul태그가 만들어진다.
     constructor() {
-        this.element = document.createElement('ul');
-        this.element.setAttribute('class', 'page');
-        this.element.textContent = 'This is PageComponent';
-
+        super('<ul class="page">This is pageComponent</ul>')
         
     }
 
     // PageComponent에는 attachTo라는 메서드가 있다.
     // parent라는 요소를 전달해주면, 내부적으로 element를 parent에 넣어준다.
-    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
-        parent.insertAdjacentElement(position, this.element);
-        //insertAdjacentElement: innerHTML과 비슷하지않을까?
-    }
+   
 }
